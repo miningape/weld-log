2a9d5b3d9f40e4a02eb15ebab52d149cd46e77ea
Kyle <kyle@weld.app>
2021-12-06 11:07:13 +0100

    removed ignored

--- bin/test.js
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env node
-"use strict";
-console.log('{"level":30,"time":1638271995380,"pid":12045,"hostname":"Kyles-MBP","context":"RouterExplorer","msg":"Mapped {/connections/shopify, GET} route"}');
